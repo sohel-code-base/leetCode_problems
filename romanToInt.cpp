e8158316a3a1f84d78dd6ee89c1fa8e1fa4adff2
soheldeveloper <sohelrananayanpur@gmail.com>
2022-05-05 13:42:51 +0600

    convert roman number to integer

---
+++ romanToInt.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+class Solution {
+public:
+int romanToInt(string S) {
+        int ans = 0, num = 0;
+        for (int i = S.size()-1; ~i; i--) {
+            switch(S[i]) {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+            if (4 * num < ans) ans -= num;
+            else ans += num;
+        }
+        return ans;        
+    }
+};
+
+
+int main(){
+    Solution solution;
+    cout << solution.romanToInt("MCMXCIV") << endl;
+
+    return 0;
+}
